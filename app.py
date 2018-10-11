c88de2a919c6ff35e0ad01114917c78f372709bb
inferno-org <teamblaze@gmail.com>
2018-10-11 00:47:16 -0500

    First commit - working flask server that has 4 main pages working

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 
-
-@app.route('/')
-def hello():
+@app.route('/hello')
+def home():
     return "Hello World!"
 
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/mission.html')
+def mission():
+    return render_template('mission.html')
+
+@app.route('/teams.html')
+def teams():
+    return render_template('teams.html')
+
+@app.route('/sponsors.html')
+def sponsors():
+    return render_template('sponsors.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
