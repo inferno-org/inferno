7893b9a50dbf5aa6006cc2264fc8186f8184fb55
inferno-org <teamblaze0402@gmail.com>
2019-07-09 17:52:59 -0500

    Added basic login functionality. Reworked the team pages. Preparing for multiple games.

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, session, flash
+from functools import wraps
 from flask_login import LoginManager, login_required
-from flask import send_from_directory
-from flask import render_template
 from flask_bootstrap import Bootstrap
-from flask import request
 from werkzeug.urls import url_parse
 #from app import db
 #from app.forms import RegistrationForm
 
 app = Flask(__name__)
 Bootstrap(app)
-login = LoginManager(app)
-login.login_view = 'login'
+app.secret_key = 'NW2clYRvJVwBEfoIqvoJnQGgufgwreY9'
+
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -22,67 +21,68 @@ def favicon():
 def start():
     return render_template('index.html')
 
-@app.route('/index.html')
+@app.route('/home')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/mission.html')
+@app.route('/mission')
 def mission():
     return render_template('mission.html')
 
-@app.route('/teams.html')
+@app.route('/teams')
 def teams():
     return render_template('teams.html')
 
-@app.route('/sponsors.html')
+@app.route('/sponsors')
 def sponsors():
     return render_template('sponsors.html')
 
-@app.route('/blaze.html')
+@app.route('/blaze')
 def blaze():
     return render_template('blaze.html')
 
-@app.route('/ember.html')
+@app.route('/ember')
 def ember():
     return render_template('ember.html')
 
-@app.route('/ragnarok.html')
+@app.route('/streams')
+def streams():
+    return render_template('streams.html')
+
+@app.route('/ragnarok')
 def ragnarok():
     return render_template('ragnarok.html')
 
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    #...
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    # ...
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home'))
+    return render_template('login', error=error)
 
-@login_required
-@app.route('/profile.html')
-def profile():
-    return render_template('profile.html')
-
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('home'))
 if __name__ == '__main__':
 
     app.run(debug=True, port=33507)
